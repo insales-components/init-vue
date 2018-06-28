b7527dd56d432940688c481038aec2b701b5788c
Vladimir Ivanin <ivaninvv@mail.ru>
2018-06-28 16:45:56 +0300

    doc

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 Инициализация Vue.js и компонентов магазина.
 
 Контейнер приложения *#insales-theme*
+
+## Пример компонента
+
+```js
+InSalesVueComponents['logo'] = {
+  props: {
+    image: String,
+    title: String
+  },
+  template: '<img v-if="image" :src="image" /><span v-else>{{ title }}</span>'
+}
+```
+
+```twig
+<logo image="{{ 'logo.svg' | asset_url_if_exists }}" title="{{ account.title }}"></logo>
+```
--- vue-sales.js
+++ vue-sales.js
@@ -1,3 +1,17 @@
+/**
+ * InSalesVueComponents - Объект для добавления компонетов
+ InSalesVueComponents['logo'] = {
+   props: {
+     image: String,
+     title: String
+   },
+   template: '<img v-if="image" :src="image" /><span v-else>{{ title }}</span>'
+ }
+
+ liquid
+ <logo image="{{ 'logo.svg' | asset_url_if_exists }}" title="{{ account.title }}"></logo>
+*/
+
 if(!InSalesVueComponents) var InSalesVueComponents = {};
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -6,23 +20,25 @@ document.addEventListener("DOMContentLoaded", () => {
     return;
   }
 
+  // Добавление компонентов
   for (var name in InSalesVueComponents) {
     if (InSalesVueComponents.hasOwnProperty(name)) {
       Vue.component(name, InSalesVueComponents[name])
     }
   }
 
+  // Проверка родительского контейнера
   var $wraper = document.getElementById('insales-theme');
   if (!$wraper) {
     console.warn('На странице нет #insales-theme!');
     return false;
   }
 
+  // Инициализация Vue
   if ($wraper && !$wraper.__vue__) {
     var InsalesTheme = new Vue({
       el: '#insales-theme',
       template: ""
-    })
-    return InsalesTheme;
+    });
   }
 });
